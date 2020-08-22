cb43f288bc0a5cbd2b856e0f83423281e840d7e1
mza97 <mza97@sfu.ca>
2020-08-22 11:55:53 -0600

    1. Set up Mongo DB connection 2. Set up environment variables

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//Load environment variable
+dotenv.config({ path: "./config/config.env" });
+
+//connect database
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is working");
@@ -8,6 +16,13 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //close server and exit process
+  server.close(() => process.exit(1));
 });
